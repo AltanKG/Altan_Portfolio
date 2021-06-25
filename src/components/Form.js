92aeb8f1532e10443462ce5d2377842da2bf0c64
Altan Guc <altan.guc@colorado.edu>
2021-06-25 15:42:27 -0600

    adding skills to resume tab

--- src/components/Form.js
+++ src/components/Form.js
@@ -5,11 +5,17 @@ function Form() {
   return (
     <div className="form">
       <h3 className="headers">Enter your full name</h3>
-      <input type="text"></input>
+      <input type="text" placeholder="name here"></input>
       <h3 className="headers">Enter your email</h3>
-      <input type="email"></input>
+      <input type="email" placeholder="email here"></input>
       <h3 className="headers">Enter your message here</h3>
-      <textarea id="message" name="message" rows="8" cols="50"></textarea>
+      <textarea
+        id="message"
+        name="message"
+        rows="8"
+        cols="50"
+        placeholder="message here..."
+      ></textarea>
       <button id="btn">Submit</button>
     </div>
   );
--- src/components/Resume.js
+++ src/components/Resume.js
@@ -1,12 +1,34 @@
 import React from "react";
-// import "../styles/AboutMe.css";
+import "../styles/Resume.css";
 
 function ContactMe() {
   return (
-    <aside id="resume" class="section">
-      <h2>Resume</h2>
-      <p>Resume will be here later</p>
-    </aside>
+    <>
+      <aside id="resume" class="section">
+        <h2>Resume</h2>
+        <p>Resume will be here later</p>
+      </aside>
+
+      <div id="container">
+        <h2 id="header2">Software Developer Skills</h2>
+        <div className="list">
+          <li>HTML5</li>
+          <li>CSS3</li>
+          <li>JavaScript</li>
+          <li>jQuery</li>
+          <li>Node.js</li>
+          <li>Express.js</li>
+        </div>
+        <div className="list">
+          <li>React.js</li>
+          <li>PHP</li>
+          <li>MongoDB</li>
+          <li>MySQL</li>
+          <li>Git</li>
+          <li>Command Line</li>
+        </div>
+      </div>
+    </>
   );
 }
 
