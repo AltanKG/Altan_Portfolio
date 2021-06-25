124eb71770db2b78544fbee53fde9e1ea4866da9
Altan Guc <altan.guc@colorado.edu>
2021-06-25 15:14:41 -0600

    adding readme and form

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,16 +1,16 @@
 import React from "react";
-// import "../styles/Form.css";
+import "../styles/Form.css";
 
 function Form() {
   return (
-    <div>
-      <h3>Enter your full name</h3>
+    <div className="form">
+      <h3 className="headers">Enter your full name</h3>
       <input type="text"></input>
-      <h3>Enter your email</h3>
+      <h3 className="headers">Enter your email</h3>
       <input type="email"></input>
-      <h3>Enter your message here</h3>
+      <h3 className="headers">Enter your message here</h3>
       <textarea id="message" name="message" rows="8" cols="50"></textarea>
-      <button>Submit</button>
+      <button id="btn">Submit</button>
     </div>
   );
 }
